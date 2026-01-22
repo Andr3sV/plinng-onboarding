504a57a24c1bae2ffa05e4df0bb55403b8f500f5
Andr3sV <andresvillamizarq@gmail.com>
2026-01-22 11:14:09 +0100

    feat: reducir padding horizontal del contenedor principal
    - Cambiar padding horizontal de 112px a 1rem (px-4) en pantallas grandes
    - Mejora visualización en pantallas pequeñas manteniendo consistencia

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
                 <div className="flex min-h-screen">
                     <Sidebar />
                     <main className="flex-1 lg:pl-64 w-full">
-                        <div className="px-4 py-4 sm:px-6 sm:py-6 lg:px-[112px] lg:py-8 max-w-7xl mx-auto min-h-full">
+                        <div className="px-4 py-4 sm:px-6 sm:py-6 lg:px-4 lg:py-8 max-w-7xl mx-auto min-h-full">
                             {children}
                         </div>
                     </main>
